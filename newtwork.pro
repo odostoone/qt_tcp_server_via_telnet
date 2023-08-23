a0a27c43c5e027a5958aedb30d0ee9ccbe584c89
odostoone <qt.jojo@tutanota.com>
2023-08-24 01:56:41 +0200

    created a custom client class and debug things

--- newtwork.pro
+++ newtwork.pro
@@ -9,11 +9,13 @@ CONFIG += c++11
 #DEFINES += QT_DISABLE_DEPRECATED_BEFORE=0x060000    # disables all the APIs deprecated before Qt 6.0.0
 
 SOURCES += \
+    client.cpp \
     main.cpp \
     server.cpp \
     widget.cpp
 
 HEADERS += \
+    client.h \
     server.h \
     widget.h
 
--- server.h
+++ server.h
@@ -8,50 +8,8 @@
 #include <QRegularExpression>
 #include <QMap>
 #include <functional>
+#include "client.h"
 
-class Client : public QObject
-{
-    Q_OBJECT
-public:
-    explicit Client(QTcpSocket * socket, QObject * parent = nullptr);
-
-    QTcpSocket * new_socket = nullptr;
-
-public slots:
-    void start_read();
-
-
-public:
-    void show_my_own_connection();
-    void send_private_message(int targetIndex, const QString &message);
-    void send_to_all(const QString &message);
-
-    QString user_name;
-    void set_name(const QString &username);
-
-    void show_help();
-    void send_private_to_name(const QString&username, const QString &message);
-    void show_date();
-
-    void closed_client_connection();
-    void user_disconect();
-
-    QTimer * timer = nullptr;
-
-    int sec = 0;
-    int min = 0;
-    int hour = 0;
-
-
-
-    void timer_start();
-    void timer_stop();
-    void timer_reset();
-    void show_timer_time();
-
-
-
-};
 
 class Server : public QObject
 {
